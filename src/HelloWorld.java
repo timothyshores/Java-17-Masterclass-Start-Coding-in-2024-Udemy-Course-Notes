e71f274267cf5fd30c3729c99f9af3f0c0916181
Timothy Shores <timothymshores@gmail.com>
2024-06-07 22:32:28 -0600

    Add HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+// public is an access modifier keyword
+public class HelloWorld {
+    // main is a method that's the starting part of the program
+    // void means no return statement
+    // (String[] args) are the parameters
+    public static void main(String[] args) {
+        // Equivalent to console.log() in JS or print() in Python
+        System.out.print("Hello World");
+    }
+}
